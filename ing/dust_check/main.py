09e393eda3a54d762817077cc461c597c713a266
april2nds <71058075+april2nds@users.noreply.github.com>
2021-01-01 17:26:01 +0900

    Update main.py

--- ing/dust_check/main.py
+++ ing/dust_check/main.py
@@ -1,10 +1,9 @@
 from time import sleep
-from gpiozero import LED
+#from gpiozero import LED
 from dustDB import DustDB
 from dustAPI import DustAPI
 
 def check_dust() :
-    R_LED = LED(2)
 
     db_instance = DustDB().get_DB_data()
     api_instance = DustAPI().get_API_data()
@@ -12,12 +11,11 @@ def check_dust() :
     #공기가 좋을 때
     #내부 pm2.5 <= 15, pm10 <= 30
 
-    if db_instance['pm10'] <= 30 or db_instance['pm25'] <= 15 :
-        R_LED.off()
+    if db_instance['pm10'] > api_instance['pm10'] or db_instance['pm25'] > api_instance['pm25'] :
+        #LED.off()
+        #print("환기 필요")
     else :
-        R_LED.on()
-        print(db_instance)
-
+        #LED.off()
 
 if __name__ == '__main__' :
 
