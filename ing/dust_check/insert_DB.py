97539389ca8914d61b8d004681dac6226ab0fe66
april2nds <71058075+april2nds@users.noreply.github.com>
2021-01-02 23:31:08 +0900

    Update insert_DB.py

--- ing/dust_check/insert_DB.py
+++ ing/dust_check/insert_DB.py
@@ -1,28 +1,24 @@
 import requests
 import serial
 from time import sleep
-from bs4 import BeautifulSoup
 
-URI = "https://api.thingspark.kr/channels/entrys?apiKey='API KEY'"
+url = "https://api.thingspark.co.kr/update"
 
-count = 0
-
+params = {
+        "apiKey" : "n1bhXYshCWi3lVHW"
+}
 ser = serial.Serial('/dev/ttyAMA0')
-result_stm = dict()
-add_pm10 = "&field1="
-add_pm25 = "&field2="
+
 while True :
     record = ser.readline()
     record = record[:-2].decode()
     record = record.split(',')
 
-    result_stm['pm10'] = record[0]
-    result_stm['pm25'] = record[1]
+    params['field1'] = record[0]
+    params['field2'] = record[1]
 
-    response = requests.get(URI + add_pm10 + result_stm['pm10'] + add_pm25 + result_stm['pm25'])
-
-    soup = BeautifulSoup(response.text, 'html.parser')
+    r = requests.get(url, params=params)
 
 #    count = count + 1
-    print(result_stm)
+    print(params)
     sleep(31)
